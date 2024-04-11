8aca1e3c98a29f6a02bfe0e75ed2d855cd8ab071
gtanchak <ghtanchak77@gmail.com>
2024-04-11 23:23:25 +0530

    added pwa configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useSWR from "swr";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/todos",
+    fetcher
+  );
+
+  if (isLoading) {
+    return <h1>Loading..</h1>;
+  }
 
   return (
     <>
@@ -16,20 +25,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.map((todo) => (
+        <p>{todo.title}</p>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

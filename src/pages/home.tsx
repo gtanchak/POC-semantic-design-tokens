e301915de20d0964a719ee6c69b653de398d1506
gtanchak <ghtanchak77@gmail.com>
2024-10-15 18:35:11 +0530

    configure the tailwind css

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,38 +1,38 @@
 const HomePage = () => {
   return (
-    <div className='bg-grey-5 min-h-screen py-6 sm:py-12'>
+    <div className='bg-subtle min-h-screen py-6 sm:py-12'>
       <div className='mx-auto max-w-4xl p-6 sm:p-8'>
-        <div className='border border-grey-20 bg-grey-0 p-8 rounded'>
-          <span className='uppercase text-xs leading-4 tracking-wider text-grey-40'>
+        <div className='border border-muted bg-neutral p-8 rounded'>
+          <span className='uppercase text-xs leading-4 tracking-wider text-muted'>
             Special Deal
           </span>
-          <h1 className='text-3xl font-bold leading-9 mt-1 mb-4 text-grey-100'>
+          <h1 className='text-3xl font-bold leading-9 mt-1 mb-4 text-copy'>
             Opportunity of a lifetime
           </h1>
-          <p className='text-grey-60'>
+          <p className='text-subtle'>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
             sint dicta quas explicabo rem quisquam consectetur aperiam facere
             temporibus aut. Voluptatibus minus nesciunt qui voluptas fugiat
             voluptate repudiandae error asperiores.
           </p>
-          <button className='px-4 py-2 rounded bg-grey-80 text-grey-5 hover:bg-grey-100 font-normal text-sm mt-6'>
+          <button className='px-4 py-2 rounded bg-bold text-inverted hover:bg-grey-100 font-normal text-sm mt-6'>
             Learn more
           </button>
         </div>
         <div className='flex gap-8 mt-8'>
-          <div className='px-8 py-12 rounded bg-teal text-center flex-1'>
-            <span className='uppercase text-sm tracking-wide leading-5 text-grey-100/60'>
+          <div className='px-8 py-12 rounded bg-highlight text-center flex-1'>
+            <span className='uppercase text-sm tracking-wide leading-5 text-copy'>
               Conversion rate
             </span>
-            <h1 className='text-6xl leading-[60px] font-black text-grey-100'>
+            <h1 className='text-6xl leading-[60px] font-black text-copy'>
               98.6%
             </h1>
           </div>
-          <div className='px-8 py-12 rounded bg-purple text-center flex-1'>
-            <span className='uppercase text-sm tracking-wide leading-5 text-grey-5/70'>
+          <div className='px-8 py-12 rounded bg-accesent text-center flex-1'>
+            <span className='uppercase text-sm tracking-wide leading-5 text-inverted'>
               customers
             </span>
-            <h1 className='text-6xl leading-[60px] text-grey-5 font-black'>
+            <h1 className='text-6xl leading-[60px] text-inverted font-black'>
               1M+
             </h1>
           </div>
